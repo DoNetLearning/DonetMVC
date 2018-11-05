b544941ba3ab3eccbdd6f214b6fe5022dafd06d5
zxlb <zhangawr@gmail.com>
2018-11-05 16:32:37 +0800

    10.3创建Home的BLL层和DAL层

---
+++ App.Admin/App/BLL/HomeBLL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using App.IBLL;
+using App.IDAL;
+using App.Models;
+using Unity.Attributes;
+
+namespace App.BLL
+{
+    public class HomeBLL : IHomeBLL
+    {
+        [Dependency]
+        public IHomeRepository HomeRepository { get; set; }
+
+        public List<SysModule> GetMenuByPersonId(string moduleId)
+        {
+            return HomeRepository.GetMenuByPersonId(moduleId);
+        }
+    }
+}
---
+++ App.Admin/App/DAL/HomeRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using App.IDAL;
+using App.Models;
+
+namespace App.DAL
+{
+    public class HomeRepository : IHomeRepository, IDisposable
+    {
+        public List<SysModule> GetMenuByPersonId(string moduleId)
+        {
+            using (AppsDBContainer db = new AppsDBContainer())
+            {
+                var menus =
+                (
+                    from m in db.SysModule
+                    where m.ParentId == moduleId
+                    where m.Id != "0"
+                    select m
+                ).Distinct().OrderBy(a => a.Sort).ToList();
+                return menus;
+            }
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
