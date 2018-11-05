49a9dd4944f3c46c77319be2e04f22146b87a130
zxlb <zhangawr@gmail.com>
2018-11-05 17:55:12 +0800

    11.1添加用户类AccountModel

--- App.Admin/Controllers/AccountController.cs
+++ App.Admin/Controllers/AccountController.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using App.Models.Sys;
 
-namespace App.Admin.Models
+namespace App.Admin.Controllers
 {
     public class AccountController : Controller
     {
+        AccountModel account = new AccountModel();
+      
         //
         // GET: /Account/
 
         public ActionResult Index()
         {
+            account.Id = "admin";
+            account.TrueName = "admin";
+            Session["Account"] = account;
             return View();
         }
 
